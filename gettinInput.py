75f9c1dde262fb2d541a8645982d86488faa8a28
ROHANI <rohani1996@gmail.com>
2020-01-09 17:15:41 -0500

    Add files via upload

---
+++ gettinInput.py
@@ -0,0 +1,5 @@
+# getting user input
+name = input("enter your name: ") # storing the value the user input and store it in the container called name
+age = input('whats your age?: ')
+
+print("hello " + name + age)
