3ebe5f7ca4ee88c09b1fc9286b918739d1d4dd67
ROHANI <rohani1996@gmail.com>
2020-01-09 17:47:40 -0500

    Add files via upload

---
+++ tuplesPython.py
@@ -0,0 +1,9 @@
+# type of data structure/ container that sores values
+
+coordinates = (4, 5) # tuples with coordinates inside of it
+# tuples are immutable ==> cant be changed or modified once created
+print(coordinates[0]) # will print 4
+
+# difference between tuples and a list-------------------------------
+# can change lists
+# use tuples for data that never change like coordinates 
