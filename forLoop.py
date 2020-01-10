56e7a5877b7db03bf136757003f759f551a5986c
ROHANI <rohani1996@gmail.com>
2020-01-09 21:04:12 -0500

    Add files via upload

---
+++ forLoop.py
@@ -0,0 +1,21 @@
+# for Loop
+
+for letter in "giraffe academy": 
+    print(letter)
+
+friends =['jim', 'karen', 'kevin'] # for each friend inside of this friends array, i wanna print out the friends
+for friend in friends:
+    print(friend)
+
+friends =['jim', 'karen', 'kevin']
+#len(friends) # to know length of the array
+for index in range(len(3, 10)): # will print out all the numbers from 3-10 not including 10
+    print(friends[index]) # print out all the elements
+
+
+for index in range(5):
+    if index == 0;
+        print("first iteration")
+    else:
+        print('not first')
+        
